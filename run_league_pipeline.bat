738ed94d7de1d00ed7a759db2bb5ab80da38cdd0
guyeli31 <guyeli31@gmail.com>
2025-12-09 22:19:58 +0200

    Auto-update leagues

--- run_league_pipeline.bat
+++ run_league_pipeline.bat
@@ -4,18 +4,18 @@ echo Running LeagueMain.m in MATLAB...
 echo ===============================================
 
 REM Run MATLAB script and wait until it finishes
-matlab -nosplash -nodesktop -wait -r "run('G:\My Drive\Matlab_Proj\Olds\ShabiLeague\Git\ShabiIsrael\LeagueMain.m'); exit"
+matlab -nosplash -nodesktop -wait -r "run('G:\\My Drive\\Matlab_Proj\\Olds\\ShabiLeague\\Git\\ShabiIsrael\\LeagueMain.m'); exit"
 
 echo.
 echo ===============================================
 echo MATLAB finished. Running update_leagues.bat...
 echo ===============================================
 
-REM Change directory to the batch folder
+REM Change to the batch directory (IMPORTANT: /d switches drive)
 cd /d "G:\My Drive\Matlab_Proj\Olds\ShabiLeague\Git\ShabiIsrael"
 
 REM Run the update_leagues batch file
-call update_leagues.bat
+call "update_leagues.bat"
 
 echo.
 echo ===============================================
---
+++ test_hotkey.bat
@@ -0,0 +1,15 @@
+@echo off
+echo ================================
+echo TEST BATCH FILE – HOTKEY CHECK
+echo ================================
+
+echo Current directory:
+cd
+
+echo Creating test file...
+echo This is a test file created by batch. > test_output.txt
+
+echo Done!
+echo ================================
+echo Press any key to exit...
+pause > nul
