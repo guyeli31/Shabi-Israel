9aa6f0ef31adbbc20aed416873c1bf32b5c0eb5a
guyeli31 <guyeli31@gmail.com>
2025-12-09 21:54:11 +0200

    Auto-update leagues

---
+++ run_league_pipeline.bat
@@ -0,0 +1,24 @@
+@echo off
+echo ===============================================
+echo Running LeagueMain.m in MATLAB...
+echo ===============================================
+
+REM Run MATLAB script and wait until it finishes
+matlab -nosplash -nodesktop -wait -r "run('G:\My Drive\Matlab_Proj\Olds\ShabiLeague\Git\ShabiIsrael\LeagueMain.m'); exit"
+
+echo.
+echo ===============================================
+echo MATLAB finished. Running update_leagues.bat...
+echo ===============================================
+
+REM Change directory to the batch folder
+cd /d "G:\My Drive\Matlab_Proj\Olds\ShabiLeague\Git\ShabiIsrael"
+
+REM Run the update_leagues batch file
+call update_leagues.bat
+
+echo.
+echo ===============================================
+echo All tasks completed successfully.
+echo ===============================================
+pause
